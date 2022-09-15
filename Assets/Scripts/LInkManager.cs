572fb8f0c766faf3519efdd83b3761f95c041a04
Shonubi Samuel <samuelshonubi860@gmail.com>
2022-09-15 12:54:09 +0100

    First commit

---
+++ Assets/Scripts/LInkManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LInkManager : MonoBehaviour
+{
+    public void Youtube(){
+    	Application.OpenURL("www.youtube.com");
+    }
+    
+    public void Instagram(){
+    	Application.OpenURL("www.instagram.com");
+    }
+    
+    public void LinkedIn(){
+    	Application.OpenURL("www.linkedin.com");
+    }
+    
+    public void Twitter(){
+    	Application.OpenURL("www.twitter.com");
+    }
+}
